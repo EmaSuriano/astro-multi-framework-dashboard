d0d1cd301521c48fc770e3229deb5cd95e772e69
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-09-21 18:50:48 +0200

    adding sidebar with react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emasuriano.github.io',
   base: '/astro-multi-framework-dashboard',
+  integrations: [tailwind(), react(), vue(), svelte()],
 });
