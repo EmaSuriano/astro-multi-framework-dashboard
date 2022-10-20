05676fb1096be2c2ff8be8cbc2e02a0da16a6fe2
Emanuel Suriano <emanuel.suriano@revolut.com>
2022-10-20 17:48:47 +0200

    add container ui

--- src/components/Highlighter.tsx
+++ src/components/Highlighter.tsx
@@ -21,7 +21,7 @@ export const HighlighterToggle = () => {
     <div className="tw-text-white">
       <button
         type="button"
-        className="tw-absolute tw-right-10 tw-transition-all tw-drop-shadow-lg hover:tw-drop-shadow-xl tw-bottom-10 tw-text-white tw-bg-blue-500 hover:tw-bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-300 tw-font-medium tw-rounded-full tw-text-sm tw-p-4 tw-text-center tw-inline-flex tw-items-center tw-mr-2 dark:tw-bg-blue-600 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-800"
+        className="tw-absolute tw-opacity-50 hover:tw-opacity-100 tw-right-10 tw-transition-all tw-drop-shadow-lg hover:tw-drop-shadow-xl tw-bottom-10 tw-text-white tw-bg-blue-500 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-300 tw-font-medium tw-rounded-full tw-text-sm tw-p-4 tw-text-center tw-inline-flex tw-items-center tw-mr-2 dark:tw-bg-blue-600 dark:hover:tw-bg-blue-700 dark:focus:tw-ring-blue-800"
         onClick={() => isFrameworkVisible.set(!visible)}
       >
         <svg
@@ -34,7 +34,7 @@ export const HighlighterToggle = () => {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            stroke-width="2"
+            strokeWidth="2"
             d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
           ></path>
           <path
--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,5 +1,4 @@
 import type { App } from 'vue';
-import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
